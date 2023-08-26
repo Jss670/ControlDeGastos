ae2bdf12c539d3b30715c39a64bf2b58dd4db6b8
Octavio Garcia <jss670@gmail.com>
2023-08-25 19:45:49 -0700

    Creando Clases y Atributos

---
+++ ControlDeGastos/ControlDeGastos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlDeGastos
+{
+    internal class ControlDeGastos
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        public string Descripcion { get; set; } = string.Empty;
+
+        public double Importe { get; set; } = string.Empty;
+
+        public  
+
+
+
+
+
+
+    }
+}
